8d0b4ee1413ca6ea4330af88209839bb04ba9862
timpur <timothy@purchas.com>
2017-10-17 12:35:07 +1100

    Minor changes

--- src/app/components/reports/management/management.component.ts
+++ src/app/components/reports/management/management.component.ts
@@ -5,13 +5,13 @@ import { Router } from '@angular/router';
 
 import { SharedService } from '../../../services/shared/shared.service';
 
-import { ReportapiService } from '../../../services/api/report/reportapi.service';
+import { ReportAPIService } from '../../../services/api/report/reportapi.service';
 import { ReportSearchDialogComponent } from '../searchdialog/searchdialog.component'
 
 @Component({
-  selector: 'app-management',
-  templateUrl: './management.component.html',
-  styleUrls: ['./management.component.css']
+    selector: 'app-management',
+    templateUrl: './management.component.html',
+    styleUrls: ['./management.component.css']
 })
 export class ReportManagementComponent implements OnInit {
 
@@ -24,7 +24,7 @@ export class ReportManagementComponent implements OnInit {
         this.shared.setTitle("Report Management");
     }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
